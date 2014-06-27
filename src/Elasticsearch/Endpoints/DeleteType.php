b07f6f7a6b1e73e5f3bc406dd2306ab7158be1f8
Andy Burton <andy@andyburton.co.uk>
2014-06-27 12:30:53 +0100

    Delete by type
    Added Client->deleteType method and DeleteType endpoint to allow index
    type deletion.

---
+++ src/Elasticsearch/Endpoints/DeleteType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Elasticsearch\Endpoints;
+
+use Elasticsearch\Endpoints\AbstractEndpoint;
+use Elasticsearch\Common\Exceptions;
+
+/**
+ * Class DeleteType
+ *
+ * @category Elasticsearch
+ * @package Elasticsearch\Endpoints
+ * @author   Andy Burton <andy@andyburton.co.uk>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
+
+class DeleteType extends AbstractEndpoint
+{
+    /**
+     * @throws \Elasticsearch\Common\Exceptions\RuntimeException
+     * @return string
+     */
+    protected function getURI()
+    {
+		
+        if (isset($this->index) !== true) {
+            throw new Exceptions\RuntimeException(
+                'index is required for Delete'
+            );
+        }
+        if (isset($this->type) !== true) {
+            throw new Exceptions\RuntimeException(
+                'type is required for Delete'
+            );
+        }
+
+        return '/' . $this->index . '/' . $this->type;
+    }
+
+
+    /**
+     * @return string[]
+     */
+    protected function getParamWhitelist()
+    {
+        return array(
+            'consistency',
+            'parent',
+            'refresh',
+            'replication',
+            'routing',
+            'timeout',
+            'version',
+            'version_type',
+        );
+    }
+
+
+    /**
+     * @return string
+     */
+    protected function getMethod()
+    {
+        return 'DELETE';
+    }
+}
